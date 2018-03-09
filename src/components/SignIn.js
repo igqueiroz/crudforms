ccc4374a9f35a4169c75d9e54d90184267ed9922
igqueiroz <andyarm@gmail.com>
2018-03-09 20:46:02 -0300

    v3

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,14 +1,14 @@
 // Arquivo de Login para o Firebase
 
-import React, {Component}  from 'react'
-import IncludeMap from './IncludeMap'
+import React, { Component }  from 'react'
+import firebase from 'firebase'
 
 export default class SignIn extends Component {
     constructor(props) {
         super(props);
         this.state = {
             username: '',
-            password: '',
+            password: ''
         };
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
@@ -23,12 +23,28 @@ export default class SignIn extends Component {
     // usa a Geo Location do navegador e envia os dados para o estado do componente 
     handleSubmit(event) {
         event.preventDefault();
+        firebase.initializeApp({
+            authDomain: "paguemob-interview-environment.firebaseapp.com",
+        })
+        firebase.auth().doSignInWithEmailAndPassword(this.state.username, this.state.password)
+             .then(() => {
+                 alert('Email validado')
+             })
+            .catch(function(error) {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                if (errorCode === 'auth/wrong-password') {
+                    alert('Wrong password.');
+                } else {
+                    alert(errorMessage);
+                }
+                    console.log(error);
+            });
+        
         //alert("Desculpe, você deve ativar o serviço de Geolocation para continuar.");
-        document.querySelector('.locate').className = 'locate progress';     
+        document.querySelector('.locate').className = 'locate progress';
     }
-
-    // faz alertas de erro para o usuário
-   
 
     render() {
         return (
@@ -40,23 +56,18 @@ export default class SignIn extends Component {
                         <form id="form-contato" onSubmit={this.handleSubmit}>
                             <div className="col-xs-6 extras">
                                 <input required name="username" id="username" type="text" className="input" placeholder="Name" ref="Login" autoComplete="off" value={this.state.username} onChange={this.handleChange}   />
-                                    <label htmlFor="txtFullname">Username</label>
+                                <label htmlFor="txtFullname">Username</label>
                             </div>
                             <div className="col-xs-6 extras">
                                 <input required name="email" id="password" type="password" className="input" placeholder="Key" ref="email" autoComplete="off" value={this.state.password}  onChange={this.handleChange} />
-                                    <label htmlFor="txtEmail">Password</label>
+                                <label htmlFor="txtEmail">Password</label>
                              </div>
-                            
                             <div className="col-xs-4 extras" />
-                                
-                         
                             <div className="col-xs-4 extras">
                                 <button className="locate" type="Submit"> Login </button>
                             </div>
                             <div className="col-xs-4 extras" />
                                 
-                        
-                            {/*this.state.rangeButton && <button className="locate" type="Submit">Eu quero achar universidades!</button>*/}
                         </form>
                     </div>
                 </div>
